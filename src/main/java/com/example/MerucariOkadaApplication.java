e0ae4d54a89e6593c05a0230fa423b5519329077
okadayuri <chemistory.12@gmail.com>
2020-04-01 15:10:10 +0900

    aplication.ymlでDB接続.

---
+++ src/main/java/com/example/MerucariOkadaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerucariOkadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerucariOkadaApplication.class, args);
+	}
+
+}
